2effad8511f932747682567fc562d9bb5c410a90
Michal Lester <michal.lester@allegro.pl>
2020-04-04 10:08:28 +0200

    add initial projects list

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
+export interface Project {
+  id: string;
+}
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): Project[] {
+    return [
+      {id: '1'},
+      {id: '2'},
+      {id: '3'},
+      {id: '4'},
+      {id: '5'},
+      {id: '6'},
+      {id: '7'},
+      {id: '8'},
+      {id: '9'},
+      {id: '10'},
+      {id: '11'},
+      ];
   }
 }
