ef79ba1ff54aca71f007c55b57b388863bbe6ad5
michal.lester87@gmail.com <michal.lester87@gmail.com>
2020-04-15 10:57:30 +0200

    add mocked mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
 
+const testUri = 'mongodb://mo1272_kodifaj:Fender15!@195.167.159.159:27017/mo1272_kodifaj';
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URI, {
+    MongooseModule.forRoot(process.env.MONGO_URI || testUri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
