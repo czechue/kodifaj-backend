8883c152cee93e62b5eca1c271bd84a19ca0d06c
michal.lester87@gmail.com <michal.lester87@gmail.com>
2020-04-15 13:18:32 +0200

    mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
 
-const testUri = 'mongodb://mo1272_kodifaj:Fender15!@195.167.159.159:27017/mo1272_kodifaj';
+// const testUri = 'mongodb://mo1272_kodifaj:Fender15!@195.167.159.159:27017/mo1272_kodifaj';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URI || testUri, {
+    MongooseModule.forRoot(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
